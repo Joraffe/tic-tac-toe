9d22c48d520c3b8252327bb24012e7b5d049ad76
Jordan Medina <jordanwmedina@gmail.com>
2017-03-13 11:21:18 -0700

    (feat) Add printBoard function

--- tictactoe.js
+++ tictactoe.js
@@ -7,3 +7,23 @@ function parseBoard(string) {
   // Return an array of arrays
   return board;
 }
+
+function printBoard(arr) {
+  var str = '';
+  for (var i = 0; i < arr.length; i++) {
+    var row = arr[i];
+    for (var j = 0; j < row.length; j++) {
+      var spot = row[j];
+      if (!spot) {
+        str += ' ';
+      } else {
+        str += spot;
+      }
+      if (j !== row.length - 1) {
+        str += '|';
+      }
+    }
+    str += '\n';
+  }
+  return str;
+}
